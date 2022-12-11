e28a0acacde0dc3da99423acf0592d7ed5c7c730
Vladyslav Varvianskyi <pol92leon@gmail.com>
2022-12-11 13:16:40 +0200

    Add components, import components, add scss components files

---
+++ src/components/timer/Timer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Timer.module.scss';
+
+const Timer = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Timer;
